e207815f5234519b21b59918232e799732d0c2d1
Muhammad Fabian Nurdin <118607381+ianyudiardi@users.noreply.github.com>
2023-11-25 14:40:08 +0700

    Praktikum 10 MVC

---
+++ Pemrograman Lanjut/10/PerpustakaanAppMVC/PerpustakaanAppMVC/Controller/MahasiswaController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PerpustakaanAppMVC.Model.Context;
+using PerpustakaanAppMVC.Model.Entity;
+using PerpustakaanAppMVC.Model.Repository;
+
+namespace PerpustakaanAppMVC.Controller
+{
+    public class MahasiswaController
+    {
+        private MahasiswaRepository _repository;
+        public int Create(Mahasiswa mhs)
+        {
+            int result = 0;
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                MessageBox.Show("Angkatan harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method Create class repository untuk menambahkan data
+                result = _repository.Create(mhs);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil disimpan !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal disimpan !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+
+        public int Update(Mahasiswa mhs)
+        {
+            int result = 0;
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                MessageBox.Show("Angkatan harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method Create class repository untuk menambahkan data
+                result = _repository.Update(mhs);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil disimpan !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal disimpan !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+
+        public int Delete(Mahasiswa mhs)
+        {
+            int result = 0;
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                MessageBox.Show("Angkatan harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method Create class repository untuk menambahkan data
+                result = _repository.Delete(mhs);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil disimpan !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal disimpan !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+    }
+}
